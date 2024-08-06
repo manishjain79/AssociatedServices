6a0a2ecec494de706fef8a9d8809c7f5db0fa1ab
manish-jain_nttltd <manish@NTT-12P8KR3.localdomain>
2024-08-06 20:10:43 +0800

    updated deployment CSRF token

--- tm_web_app/deployment.py
+++ tm_web_app/deployment.py
@@ -6,6 +6,6 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = [f'https://{ALLOWED_HOSTS[0]}']
 
 DEBUG = False
