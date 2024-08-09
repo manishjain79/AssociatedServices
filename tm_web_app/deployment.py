8067cac8d40ff0a39189fee7af06022d9b965277
manish-jain_nttltd <manish@NTT-12P8KR3.localdomain>
2024-08-10 07:50:50 +0800

    changed

--- tm_web_app/deployment.py
+++ tm_web_app/deployment.py
@@ -3,7 +3,7 @@ from .settings import *
 from .settings import BASE_DIR
 
 
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 
 CSRF_TRUSTED_ORIGINS = [f'https://{ALLOWED_HOSTS[0]}']
