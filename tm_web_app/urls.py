18e02169692b57d772432a33e4e9d1684690205f
manish-jain_nttltd <manish@NTT-12P8KR3.localdomain>
2024-08-12 10:31:40 +0800

    Added Home page

--- tm_web_app/urls.py
+++ tm_web_app/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 # from django.contrib import views as auth_views
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Home, name='Home'),
     path('ciAssociatedServices/', include('ciAssociatedServices.urls')),
 ]
---
+++ tm_web_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def Home(request):
+    '''
+    This function gets executed upon calling for any gcname.
+    '''
+    context = {
+        'title': 'Home'}
+    return render(request, 'tm_web_app/home.html', context)
