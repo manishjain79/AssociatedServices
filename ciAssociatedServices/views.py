c916a96d1b34a2cfcfade11fa922c9c6a67f4a7a
manishjain79 <manishjain79@hotmail.com>
2024-08-29 19:28:03 +0800

    separated header parser and created ci associated svc home page

--- ciAssociatedServices/views.py
+++ ciAssociatedServices/views.py
@@ -32,27 +32,6 @@ def get_data(xlsfile: str, xlssheet: str, filtercolumnname: str, filterstring: s
     else:
         df_filtered = df[df[filtercolumnname].isin([filterstring.upper()])]
     generated_filtered_df_as_html = generate_html(df_filtered)
-    # df_tma = df[df['GC'].isin(['TMA'])]
-    # df_tmis = df[df['GC'].isin(['TMIS'])]
-    # df_tmls = df[df['GC'].isin(['TMLS'])]
-    # df_tmlth = df[df['GC'].isin(['TMLTH'])]
-    # df_tmsth = df[df['GC'].isin(['TMSTH'])]
-    # df_tmiv = df[df['GC'].isin(['TMIV'])]
-    # df_tmlm = df[df['GC'].isin(['TMLM'])]
-    # df_tmim = df[df['GC'].isin(['TMIM'])]
-    # df_tmi = df[df['GC'].isin(['TMI'])]
-    # df_tmli = df[df['GC'].isin(['TMLI'])]
-    # tmahtml = generate_html(df_tma)
-    # tmishtml = generate_html(df_tmis)
-    # tmlshtml = generate_html(df_tmls)
-    # tmlthhtml = generate_html(df_tmlth)
-    # tmsthhtml = generate_html(df_tmsth)
-    # tmivhtml = generate_html(df_tmiv)
-    # tmlmhtml = generate_html(df_tmlm)
-    # tmimhtml = generate_html(df_tmim)
-    # tmihtml = generate_html(df_tmi)
-    # tmlihtml = generate_html(df_tmli)
-    # print(df_tma)
     return generated_filtered_df_as_html
 
 
@@ -64,21 +43,7 @@ def gethtmltemplate(cli_xlsfile, cli_xlssheet, cli_columnToFilter, cli_gcname):
     return filtered_html
 
 
-# Create your views here.
-def home(request):
-    '''
-    This function gets executed upon calling for any gcname.
-    '''
-    context = {
-        'data_table': gethtmltemplate(xls_filename,'CIs_Asscoiated Service Offering','GC', 'ALL'),
-        'title': 'Home' }
-    return render(request, 'ciAssociatedServices/gc.html', context)
-
-# Create your views here.
-def GCDetails(request, gcname):
-    '''
-    This function gets executed upon calling for any gcname.
-    '''
+def parse_header(request):
     headers = request.headers
     client_principal = headers['X-Ms-Client-Principal']
     principal_name = headers['X-Ms-Client-Principal-Name']
@@ -96,6 +61,52 @@ def GCDetails(request, gcname):
     roles = []
     for role_dict in roles_from_claim:
         roles.append(role_dict['val'])
+    return (roles, name)
+
+# Create your views here.
+def home(request):
+    '''
+    This function gets executed upon calling for any gcname.
+    '''
+    (roles, name) = parse_header(request=request)
+    gcaccess = []
+    for role in roles:
+        if role.split(".")[0] == 'TM':
+            gc = role.split(".")[1]
+            gcaccess.append(gc)
+    # # context = {
+    # #     'data_table': gethtmltemplate(xls_filename,'CIs_Asscoiated Service Offering','GC', 'ALL'),
+    # #     'title': 'Home' }
+    # return render(request, 'ciAssociatedServices/gc.html', context)
+    context = {
+        'assigned_gc': gcaccess
+    }
+    return render(request, 'ciAssociatedServices/ciassociatedservice_home.html', context)
+
+# Create your views here.
+def GCDetails(request, gcname):
+    '''
+    This function gets executed upon calling for any gcname.
+    '''
+    # headers = request.headers
+    # client_principal = headers['X-Ms-Client-Principal']
+    # principal_name = headers['X-Ms-Client-Principal-Name']
+    # # print(client_principal)
+    # # Payload is base64 encoded, let's decode it to plain string
+    # # To make sure decoding will always work - we're adding max padding ("==")
+    # # to payload - it will be ignored if not needed.
+    # client_principal_decoded = str(base64.b64decode(client_principal + "=="), "utf-8")
+    # # Payload is JSON - we can load it to dict for easy access
+    # client_principal = json.loads(client_principal_decoded)
+    # # print(client_principal)
+    # roles_from_claim = filter(lambda claim: claim['typ'] == 'roles', client_principal['claims'])
+    # name_from_claim = roles = filter(lambda claim: claim['typ'] == 'name', client_principal['claims'])
+    # name = next(name_from_claim)['val']
+    # roles = []
+    # for role_dict in roles_from_claim:
+    #     roles.append(role_dict['val'])
+    
+    (roles, name) = parse_header(request=request)
 
     found = False
     for role in roles:
